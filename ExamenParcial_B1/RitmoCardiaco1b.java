0ed513dd82b31eda98f0f5bcc80868c9ed1ae57c
Eduardo Encalada <aeencalada@Laptop-de-Eduardo.local>
2022-11-26 12:13:40 -0500

    20221126

---
+++ ExamenParcial_B1/RitmoCardiaco1b.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class RitmoCardiaco1b {
+
+	public static void main(String[] args) {
+		//Declaración
+		Scanner sc = new Scanner(System.in);
+		Short ritmoC;
+		String tipo;
+
+		// Datos de entrada
+		System.out.println("VALIDACIÓN DEL RITMO CARDIACO DE PERSONAS ADULTAS");
+		System.out.println("=================================================");
+
+		System.out.print("\nIngrese su ritmo cardiaco (pulsaciones por minuto): ");
+		ritmoC = sc.nextShort();
+
+		//Validación
+		if (ritmoC >= 60 && ritmoC <= 100) {
+			tipo = "NORMAL";
+		} else {
+			tipo = "ANORMAL";
+		}
+
+		// Resultados
+		System.out.println("\nSu ritmo cardiaco es: " + tipo);
+
+	}
+
+}
